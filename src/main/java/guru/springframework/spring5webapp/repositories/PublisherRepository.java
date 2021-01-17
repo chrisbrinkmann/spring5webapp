b50420de02dcac0638448299294e567ec91cf562
Chris Brinkmann <chris.brinkmann@us-dde.net>
2021-01-17 15:11:25 -0500

    Add BookController SpringMVC Controller class

--- src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -4,4 +4,9 @@ import guru.springframework.spring5webapp.domain.Publisher;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+    /**
+     * We just declare this interface, and then at runtime Spring Data JPA
+     * will provide the implementation, which gives us all of the CRUD
+     * stuff from CrudRepository for doing DB operations.
+     */
 }
